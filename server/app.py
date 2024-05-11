8d007fbac645f1c217323b63259eafb570a2b8c8
Timothy Ryon <tim@timryon.com>
2024-05-11 18:18:14 -0400

    wip

--- server/app.py
+++ server/app.py
@@ -1,8 +1,13 @@
 #!/usr/bin/env python3
 
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
+# restart flask server with flask run in server dir
+@app.route('/')
+def index():
+    host = request.headers.get('Host')
+    return f'<h1>The host for this page is {host}</h1>'
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
